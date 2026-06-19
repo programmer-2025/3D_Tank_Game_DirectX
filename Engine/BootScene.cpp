78d45d58cc734ad0d9bc6ea3f4c11b223ef3dcb0
programmer-2025 <snowsansub011307@gmail.com>
2026-06-19 21:28:47 +0900

    ＜追加＞　シーンを追加しました

---
+++ Engine/BootScene.cpp
@@ -0,0 +1,20 @@
+#include "BootScene.h"
+
+BootScene::BootScene()
+	: BaseScene("BootScene") {
+}
+
+BootScene::~BootScene() {
+}
+
+void BootScene::Init() {
+}
+
+void BootScene::Update() {
+}
+
+void BootScene::Draw() {
+}
+
+void BootScene::Release() {
+}
---
+++ Engine/SceneManager.cpp
@@ -0,0 +1,53 @@
+#include "SceneManager.h"
+#include <vector>
+#include "BootScene.h"
+
+namespace {
+	std::vector<BaseScene*> sceneList;
+	BaseScene* currentScene = nullptr;
+}
+
+namespace SceneManager {
+
+	void InitManager() {
+		sceneList.clear();
+		auto bootScene = new BootScene();
+		sceneList.push_back(bootScene);
+		ChangeScene("BootScene");
+	}
+
+	BaseScene* GetCurrentScene() {
+		return currentScene;
+	}
+
+	void ChangeScene(const std::string& name) {
+		for (auto& scene : sceneList) {
+			if (scene->GetName() == name) {
+				currentScene = scene;
+				currentScene->Init();
+				break;
+			}
+		}
+	}
+
+	void DrawScene() {
+		if (currentScene == nullptr) return;
+		currentScene->Draw();
+	}
+
+	void InitScene() {
+		if (currentScene == nullptr) return;
+		currentScene->Init();
+	}
+
+	void UpdateScene() {
+		if (currentScene == nullptr) return;
+		currentScene->Update();
+	}
+
+	void ReleaseScene() {
+		if (currentScene == nullptr) return;
+		currentScene->Release();
+	}
+
+}
