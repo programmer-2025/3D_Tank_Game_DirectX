ec4ba84aa49cea9722489148f620541f8fe2da96
programmer-2025 <snowsansub011307@gmail.com>
2026-07-09 21:51:45 +0900

    ＜修正＞　デフォルトの背景を白に変更

--- GameEngine.hpp
+++ GameEngine.hpp
@@ -3,7 +3,7 @@
 namespace GameEngine {
 	inline const int DEFAULT_WIDTH = 1280;
 	inline const int DEFAULT_HEIGHT = 720;
-	inline const float BACKGROUND_COLOR[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	inline const float BACKGROUND_COLOR[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
 
 	HWND GetWindowHandle();
 }
